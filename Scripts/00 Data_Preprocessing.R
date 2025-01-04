cf1502a3f8551b8fe2a6b104e2c431d13f56b055
Pritom <pritom.kundu.ru@gmail.com>
2025-01-04 06:19:59 +0600

    updated practice script

---
+++ Scripts/00 Data_Preprocessing.R
@@ -0,0 +1,8 @@
+# Doing sum of numbers
+   sum(10,17)
+# Installing Packages
+    install.packages("tidyverse")
+# Load The Needed Packages
+    library(tidyverse)
+# Taking Help from R Studio "?asking value"
+    ?tidyverse
